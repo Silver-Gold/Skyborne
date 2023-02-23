54d940827634fbbc7b1685231d7c9703c3d60888
SilverGold <mcredstonewing@gmail.com>
2023-02-23 02:04:11 -0500

    Added Stone Trees Added Stone Logs and Log Variants Added Yellow Elysian Foliage Added Andesite Deposit

--- src/main/java/net/silvergold/skyborne/block/ModBlocks.java
+++ src/main/java/net/silvergold/skyborne/block/ModBlocks.java
@@ -4,6 +4,8 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -12,6 +14,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.silvergold.skyborne.Skyborne;
+import net.silvergold.skyborne.block.custom.ModFlammableRotatedPillarBlock;
 import net.silvergold.skyborne.item.ModCreativeModeTab;
 import net.silvergold.skyborne.item.ModItems;
 
@@ -25,6 +28,12 @@ public class ModBlocks {
     public static final RegistryObject<Block> SKY_STONE = registerBlock("sky_stone", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(1.5f).requiresCorrectToolForDrops()), ModCreativeModeTab.SKYBORNE_TAB);
     public static final RegistryObject<Block> COBBLED_SKY_STONE = registerBlock("cobbled_sky_stone", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.SKYBORNE_TAB);
     public static final RegistryObject<Block> STONE_PLANKS = registerBlock("stone_planks", () -> new Block(BlockBehaviour.Properties.of(Material.WOOD).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> STONE_LOG = registerBlock("stone_log", () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> STRIPPED_STONE_LOG = registerBlock("stripped_stone_log", () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_LOG).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> STONE_WOOD = registerBlock("stone_wood", () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> STRIPPED_STONE_WOOD = registerBlock("stripped_stone_wood", () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.STRIPPED_OAK_WOOD).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> STONE_LOG_ANDESITE = registerBlock("stone_log_andesite", () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG).strength(2f).sound(SoundType.NETHER_BRICKS)), ModCreativeModeTab.SKYBORNE_TAB);
+    public static final RegistryObject<Block> YELLOW_ELYSIAN_FOLIAGE = registerBlock("yellow_elysian_foliage", () -> new LeavesBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LEAVES).sound(SoundType.AZALEA_LEAVES)), ModCreativeModeTab.SKYBORNE_TAB);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
